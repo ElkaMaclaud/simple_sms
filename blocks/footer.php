6762fb48613cde7e81d32cef332bb25c684aa717
ElkaMaclaud <elkamaclaud@gmail.com>
2025-01-27 13:47:35 +0300

    fix: corrected the layout and styles

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,4 +1,5 @@
-<footer  class="mt-5">Все права защищены &copy; 2025</footer>
+</section>
+<footer>Все права защищены &copy; 2025</footer>
 </div>
 </body> 
 </html>
--- blocks/header.php
+++ blocks/header.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/../styles/style.css">
 </head>
+
 <body>
-<div class="container mt-5">
-    <header>
-    </header>
-    
+    <div class="container">
+            <header>
+                CMS на PHP
+            </header>
+            <section class="nested_container">
