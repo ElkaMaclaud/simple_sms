c5805001f117be9aa910f579556447b41c119c64
ElkaMaclaud <elkamaclaud@gmail.com>
2025-01-28 16:53:31 +0300

    feat: added a picture to the page

--- blocks/header.php
+++ blocks/header.php
@@ -6,12 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/../styles/style.css">
+    <!-- <link rel="stylesheet" href="<?php echo $_SERVER['DOCUMENT_ROOT']; ?>/styles/styles.css"> -->
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 
 <body>
     <div class="container">
-            <header>
-                CMS на PHP
-            </header>
-            <section class="nested_container">
+        <header>
+            CMS на PHP
+        </header>
+        <section class="nested_container">
+            <img put type="image"
+                src="https://img.freepik.com/free-vector/cms-landing-page-template_52683-56270.jpg"
+                alt="Картинка с изображением CMS" class="animateImg">
+
+
+            <!-- <?php echo $_SERVER['DOCUMENT_ROOT']; ?>  -->
